2548b98b1d6ae4e75a7241920f0a42f346238a3e
Anshuman Rathore <104229713+anshumanrathoree@users.noreply.github.com>
2022-05-12 03:54:25 +0530

    Added solution - LeetHub

---
+++ Minimum swaps and K together - GFG/minimum-swaps-and-k-together.cpp
@@ -0,0 +1,96 @@
+// { Driver Code Starts
+// C++ program to find minimum swaps required
+// to club all elements less than or equals
+// to k together
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+public:
+    int minSwap(int arr[], int n, int k) {
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<=k)
+            count++;
+        }
+        int bad=0;
+        for(int i=0;i<count;i++)
+        {
+            if(arr[i]>k)
+            bad++;
+        }
+        int ans=bad;
+        int i=0;
+        int j=count;
+       while(j<n)
+        {
+            if(arr[i]>k)
+            bad--;
+            if(arr[j]>k)
+            bad++;
+            ans=min(ans,bad);
+            i++;
+            j++;
+        }
+        return ans;
+    }
+};
+/*int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<=k)
+            count++;
+        }
+        //in the window getting the count of all the bad numbers(>k)
+        int bad=0;
+        for(int i=0;i<count;i++)
+        {
+            if(arr[i]>k)
+            bad++;
+        }
+        int ans=bad;
+        int i=0;
+        int j=count;
+       while(j<n)
+        {
+            //in the window if the excluding element was a bad element
+            if(arr[i]>k)
+            bad--;
+            
+            //in the window if the next including element is a bad element
+            if(arr[j]>k)
+            bad++;
+            ans=min(ans,bad);
+            i++;
+            j++;
+        }
+        return ans;*/
+
+
+// { Driver Code Starts.
+
+// Driver code
+int main() {
+
+	int t,n,k;
+	cin>>t;
+	while(t--)
+    {
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cin>>k;
+        Solution ob;
+        cout << ob.minSwap(arr, n, k) << "\n";
+    }
+	return 0;
+}
+  // } Driver Code Ends
